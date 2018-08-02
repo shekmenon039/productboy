a63a430d3d113e77e7a6228b262538aec0a95d80
M <684660@cognizant.com>
2018-08-02 13:17:13 +0530

    v1.7.1

--- src/app/product-list/product-detail.component.ts
+++ src/app/product-list/product-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '../../../node_modules/@angular/router';
+import { Product } from './product';
 
 @Component({
   selector: 'app-product-detail',
@@ -7,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailComponent implements OnInit {
 
+
   pageTitle: string = 'Product Detail';
-  constructor() { }
+  product: Product;
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+   let id = +this.route.snapshot.paramMap.get('id');
+   this.pageTitle +=`: ${id}`;
+   this.product =  {
+    "productId": 1,
+    "productName": "Leaf Rake",
+    "productCode": "GDN-0011",
+    "releaseDate": "March 19, 2016",
+    "description": "Leaf rake with 48-inch wooden handle.",
+    "price": 19.95,
+    "starRating": 3.2,
+    "imageUrl": "https://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
+  }
+  
   }
 
 }
